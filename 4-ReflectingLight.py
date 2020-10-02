6f668bb56c3f653b49d94c69f064506f8699cd43
Ferran <34451029+ferzu@users.noreply.github.com>
2020-10-02 16:55:15 +0200

    Update 4-ReflectingLight.py

--- 4-ReflectingLight.py
+++ 4-ReflectingLight.py
@@ -15,27 +15,26 @@ def size_index(max_x, max_y):
 
 def reflections(max_x, max_y):
     s = size_index(max_x, max_y)
-    k = int(s//1) # integer size_index. Sizes of the rectangle: max_x, max_y. Reflection 45 => max_x = k*max_y + d*max_y
+    k = int(s//1) # integer size_index. Sides of the rectangle => max_x, max_y. Reflection 45 from (0,0) => max_x = k*max_y + d*max_y
     d = s % 1 # decimal size_index
-    print (k,'\ndecimal: {}'.format(d))
     if k>1 and not d:
         if not k%2:
             return False
         else:
             return True
-    if k == 1 and not d: #square size
+    if k == 1 and not d: #square size area
         return True
     else: # While working out a vectorial solution for the reflections. When d!=0 the puzzel complicates, this solution is not accurate enough. 
         return False
 
 if __name__ == '__main__':
-    # ref = False
-    # while not ref:
-    #     max_x, max_y = (10, 10)#= r_coordenates()
-    #     print (max_x, max_y)
-    #     ref = reflections(max_x, max_y)
-    #     print (ref)
-    max_x, max_y = (45, 15)#= r_coordenates()
+#     ref = False
+#     while not ref:
+#         max_x, max_y = (10, 10)#= r_coordenates()
+#         print (max_x, max_y)
+#         ref = reflections(max_x, max_y)
+#         print (ref)
+    max_x, max_y = r_coordenates()
     print (max_x, max_y)
     ref = reflections(max_x, max_y)
     print (ref)
