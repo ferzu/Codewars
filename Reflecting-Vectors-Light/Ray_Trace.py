320a97260ac4b6a7d9c262d110064625e6728909
Ferran <34451029+ferzu@users.noreply.github.com>
2020-10-06 14:24:30 +0200

    Update Ray_Trace.py

--- Reflecting-Vectors-Light/Ray_Trace.py
+++ Reflecting-Vectors-Light/Ray_Trace.py
@@ -1,46 +1,51 @@
-
-# Working progress code, not yet 100% functional.
+# Finding exit points, vectorial.
 
 import math
-def get_reflected_vector(head, tail):
+def get_reflected_vector(head, tail, max_x, max_y):
     h0,h1 = head
     t0, t1 = tail
-    # incident vector head centered at tail (0,0)
+    # Incident vector centered at tail (0,0)
     inc = (h0-t0, h1-t1)
     ix, iy = inc
-    # normals for reflection at borders
+    # Normals for reflection at borders
     n = (0,0)
     if h1 == max_y: n = (0,-1)
     if h1 == 0: n = (0,1)
     if h0 == max_x: n = (-1,0)
     if h0 == 0: n = (1,0)
     nx,ny = n
-    print('Incidence vector: {}'.format(inc))
-    print('Normal: {}'.format(n))
-    # reflected vector head at tail (0,0)
+    # Reflected vector head at tail (0,0)
     ref  = (ix - 2*(ix*nx + iy*ny)*nx, iy - 2*(ix*nx + iy*ny)*ny)
-    # normalized reflected vector at (0,0) for shortened reflections
+    # Normalized reflected vector at (0,0) for shortened reflections
     if abs(h0 - t0) < max_y:
-        ref_u = unit_vector(ref)
-        mod = math.sqrt(2)*max_y
-        ref = tuple([mod * x for x in ref_u])
-        r_h0, r_h1 = ref
-        head = (r_h0 + h0, r_h1 + h1)
-        tail = (h0, h1)
+        ref_norm = normalize_reflection(ref, h0, h1, max_y)
+        head = ref_norm[0]
+        tail = ref_norm[1]
     else:
+        tail = head
         head = ref
-    print('Reflected vector: {}'.format(ref))
-    # print('Reflected Head: {}'.format(head))
-    # print('Reflected Tail: {}'.format(tail))
-    ref = [head, tail] # reflected vector
+    ref = [head, tail]
     return ref
 
+# Normalizing and extending shortened vectors to keep the module constant for future border crossings.
+# |ref| = |initial vector| = sqrt(2)*max_y
+def normalize_reflection(ref, h0, h1, max_y):
+    ref_u = unit_vector(ref)
+    mod = math.sqrt(2)*max_y
+    ref_norm = tuple([mod * x for x in ref_u])
+    r_h0, r_h1 = ref_norm
+    head = (round(r_h0) + h0 , round(r_h1) + h1)
+    tail = (h0, h1)
+    ref_norm = [head, tail]
+    return ref_norm
+
 def unit_vector (vector):
-    v = math.sqrt((vector[0]**2 + vector[1]**2))
+    v = math.sqrt(vector[0]**2 + vector[1]**2)
     u = (vector[0]/v, vector[1]/v)
     return u
 
-def border_reflection(head, tail):
+# Finding crossing points with borders. Crossing point = head of shortened vector. Staying inside the box.
+def border_reflection(head, tail, max_x, max_y):
     h0,h1 = head
     t0, t1 = tail
     # RIGHT BORDER
@@ -52,9 +57,7 @@ def border_reflection(head, tail):
         else: # negative slope
             h1 = h1 + d
         head = (h0, h1)
-        print('Head reduction: {}'.format(head))
-        print('Tail reduction: {}'.format(tail))
-        ref = get_reflected_vector(head,tail)
+        ref = get_reflected_vector(head,tail,max_x,max_y)
         return ref
     # TOP BORDER
     if h1 > max_y:
@@ -62,10 +65,10 @@ def border_reflection(head, tail):
         h1 = max_y
         if h0 < t0: # incidence coming from the right
             h0 = h0 + d
-        else:
+        else: # incidence coming from the left
             h0 = h0 - d
         head = (h0, h1)
-        ref = get_reflected_vector(head,tail)
+        ref = get_reflected_vector(head,tail,max_x,max_y)
         return ref
     # BOTTOM BORDER
     if h1 < 0:
@@ -73,10 +76,10 @@ def border_reflection(head, tail):
         h1 = 0
         if h0 < t0: # incidence coming from the right
             h0 = h0 + d
-        else:
+        else: # incidence coming from the left
             h0 = h0 - d
         head = (h0, h1)
-        ref = get_reflected_vector(head,tail)
+        ref = get_reflected_vector(head,tail,max_x,max_y)
         return ref
     # LEFT BORDER
     if h0 < 0:
@@ -87,56 +90,53 @@ def border_reflection(head, tail):
         else:
             h1 = h1 + d
         head = (h0, h1)
-        ref = get_reflected_vector(head,tail)
+        ref = get_reflected_vector(head,tail,max_x,max_y)
         return ref
 
-def propagate_vector(head, tail):
+# Propagates vectors with constant module (that didn't get shortened by borders)
+def propagate_vector(head, tail, max_x, max_y):
     h0,h1 = head
-    ref = get_reflected_vector(head,tail)
+    ref = get_reflected_vector(head,tail,max_x,max_y)
     p_tail = head # new tail is last head
     h00, h11 = (ref[0][0], ref[0][1])
     p_head = (h0+h00, h1+h11)
     vp = [p_head, p_tail]
-    # print(ref)
-    # print(h00,h11)
-    # print(p_head)
-    # print(vp)
     return vp
 
-def exit(head):
+# Validating conditions (0,0), (max_x, max_y): True. False otherwhise.
+def exit(head, max_x, max_y):
     if head == (0,0) or head == (max_x, max_y): return 'true'
     if head == (0,max_y) or head == (max_x,0): return 'false'
     else: return 'continue'
 
-def ray_trace(head, tail):
+# Propagating light vectors while checking conditions
+def ray_trace(head, tail, max_x, max_y):
     while True:
-        vp = propagate_vector (head, tail)
+        vp = propagate_vector (head, tail, max_x, max_y)
         head, tail = vp
         h0, h1 = head
-        print('Propagated vector: {}'.format(vp))
         while h0 > max_x or h1 > max_y or h1 < 0 or h0 < 0:
-            vr = border_reflection(head,tail)
+            vr = border_reflection(head,tail,max_x,max_y)
             head, tail = vr
             h0,h1 = head
-        print('Head: {}'.format(head))
-        exit1 = exit(head)
+        exit1 = exit(head,max_x,max_y)
         if exit1 == 'true':
-            print('True. Exit point: {}'.format(head))
             return True
             break
         if exit1 == 'false':
-            print('False. Exit point: {}'.format(head))
             return False
             break
         else:
             pass
 
-def vector_generator():
+# Generates the start vector defined by initial constraints
+def vector_generator(max_y):
     tail = (0,0)
     head = (max_y, max_y)
     v = [head, tail]
     return v
 
+# Determines the orientation of the box. Runs everything else with given box size.
 def reflections (max_x, max_y):
     if max_x == max_y:
         return True
@@ -145,18 +145,14 @@ def reflections (max_x, max_y):
         swap = max_y
         max_y = max_x
         max_x = swap
-    start = vector_generator()
-    print('Initial vector: {}'.format(start))
+    start = vector_generator(max_y)
     head, tail = start
-    print(start)
-    rt = ray_trace(head, tail)
+    rt = ray_trace(head, tail, max_x, max_y)
     return rt
 
 
 if __name__ == '__main__':
 
-    global max_x, max_y
-    max_x, max_y = (6,2)
-    # max_x > max_y
+    max_x, max_y = (10,2)
     rf = reflections(max_x, max_y)
     print(rf)
